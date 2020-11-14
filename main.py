f84e32f0483491aa097a3729e77d116e387eac28
Jack Kufa <jack@kufa.io>
2020-11-14 02:26:56 -0600

    Work on data collection script

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import string
-import nltk
+# import nltk
 import warnings
+import sys
+import csv
+import random
+import string
+
+tweet_id = 0
 
 # "@[\w]*" for twitter handles 
 # "[^a-zA-Z\s#]" for everything but letters and spaces
@@ -14,43 +20,97 @@ def remove_garbage(inpt):
   r = re.findall("@[\w]*", inpt)
   for i in r:
     inpt = re.sub(i, '', inpt)
-
   #remove links
   inpt = re.sub(r'http\S+', '', inpt)
-  
   #remove special characters
   inpt = re.sub(r'[^a-zA-Z ]+', '', inpt)
-
   #remove words with 1-2 letters
   inpt = re.sub(r'\b\w{1,2}\b', '', inpt)
-
   return inpt
 
+def is_tweet_gamer(tweet):
+  print(tweet)
+  invalid_input = True
+  while(invalid_input):
+    is_gamer = input("Is the following tweet a gamer tweet? 1 = yes, 0 = no: ")
+    if(int(is_gamer) == 0 or int(is_gamer) == 1):
+      invalid_input = False
+      return int(is_gamer)
+    else:
+      print("Error: invalid input. Please try again.")
 
-
-keys = []
-f = open("TWITTER_KEYS.txt", "r")
-for line in f:
-  keys.append(line.rstrip('\n').rsplit(': ', 1)[1])
-
-twitter = Twython(keys[0], access_token=keys[2])
-
-info = twitter.search(q='pog', tweet_mode='extended', count=200)
-
-#info = twitter.get_user_timeline(name='mitchhit234', tweet_mode='extended', count=5)
-
-#pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(info)
-
-for tweet in info['statuses']:
-#for tweet in info
-  print("Begin Tweet: ", end='')
-  if "retweeted_status" in tweet:
-    unfiltered = tweet["retweeted_status"]["full_text"]
+def get_tweets_username(twitter, username):
+  data = twitter.get_user_timeline(name=username, tweet_mode='extended', count=100)
+  for tweet in data:
+    if "retweeted_status" in tweet:
+      unfiltered = tweet["retweeted_status"]["full_text"]
+    else:
+      unfiltered = tweet["full_text"]
     filtered = remove_garbage(unfiltered)
+
+    label = is_tweet_gamer(unfiltered)
+    csv_handling(username, label, filtered, unfiltered)
+
+  
+def init_csv(username):
+    with open(username + '.csv', 'wb') as csvfile:
+      filewriter = csv.writer(csvfile, delimiter=',',
+                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
+      filewriter.writerow(['Id', 'Label', 'Unfiltered', 'Filtered'])      
+
+
+def get_tweets_random(twitter):
+  char = random.choice(string.ascii_letters)
+  data = twitter.search(q=char, tweet_mode='extended', count=500, lang='en', result_type='mixed')
+  for tweet in data['statuses']:
+    if "retweeted_status" in tweet:
+      unfiltered = tweet["retweeted_status"]["full_text"]
+    else:
+      unfiltered = tweet["full_text"]
+    filtered = remove_garbage(unfiltered)
+
+    label = is_tweet_gamer(unfiltered)
+
+    csv_handling("random", label, filtered, unfiltered)
+
+
+def csv_handling(filename, label, filt, unfilt):
+  global tweet_id
+  tweet_id += 1
+  row = str(tweet_id) + ',' + str(label) + ',' + filt + ',' + unfilt + '\n'
+  f = open(filename+'.csv', 'a')
+  f.write(row)
+  f.close()
+  
+  
+
+# global counter id
+# unfiltered
+# filtered
+  
+
+
+def main():
+  keys = []
+  f = open("TWITTER_KEYS.txt", "r")
+  for line in f:
+    keys.append(line.rstrip('\n').rsplit(': ', 1)[1])
+  twitter = Twython(keys[0], access_token=keys[2])
+  f.close()
+
+  # Check arguments
+  if len(sys.argv) > 1:
+    print(sys.argv[1])
+    username = sys.argv[1]
+    f = open(username+'.csv', 'a')
+    # TODO: Verify username exists
+    get_tweets_username(twitter, username)
   else:
-    unfiltered = tweet["full_text"]
-    filtered = remove_garbage(unfiltered)
-    print(unfiltered)
-    print(filtered)
+    f = open('random.csv', 'a')
+    print("getting random tweet")
+    get_tweets_random(twitter)
+  f.close()
+
+
+main()
 
