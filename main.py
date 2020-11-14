4cd2ea33341859cd79b3fc51472654313d933a09
Mitchell Meier <mdmfvz@mst.edu>
2020-11-14 00:33:29 -0600

    pog on my pog

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from twython import Twython
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import nltk
+import warnings
+
+# "@[\w]*" for twitter handles 
+# "[^a-zA-Z\s#]" for everything but letters and spaces
+def remove_garbage(inpt):
+  r = re.findall("@[\w]*", inpt)
+  for i in r:
+    inpt = re.sub(i, '', inpt)
+
+  #remove links
+  inpt = re.sub(r'http\S+', '', inpt)
+  
+  #remove special characters
+  inpt = re.sub(r'[^a-zA-Z ]+', '', inpt)
+
+  #remove words with 1-2 letters
+  inpt = re.sub(r'\b\w{1,2}\b', '', inpt)
+
+  return inpt
+
+
 
 keys = []
 f = open("TWITTER_KEYS.txt", "r")
@@ -7,4 +35,25 @@ for line in f:
 
 twitter = Twython(keys[0], access_token=keys[2])
 
+info = twitter.search(q='pog', tweet_mode='extended', count=200)
 
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(info)
+
+for tweet in info['statuses']:
+  print("Begin Tweet: ", end='')
+  if "retweeted_status" in tweet:
+    unfiltered = tweet["retweeted_status"]["full_text"]
+    filtered = remove_garbage(unfiltered)
+  else:
+    unfiltered = tweet["full_text"]
+    filtered = remove_garbage(unfiltered)
+    print(unfiltered)
+    print(filtered)
+
+
+#t = open("new.txt", "w")
+#for tweet in user_tweets:
+ # t.write( str(tweet) + '\n' )
+
+#t.close()
