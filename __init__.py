9f3c05f2f546f659375976972057f837fba020c8
Jack Kufa <jack@kufa.io>
2020-11-14 14:40:24 -0600

    Work on frontend

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from twython import Twython
-from model import *
+import re
+
+# from model import *
 
 app = Flask(__name__)
 app.debug=True
@@ -12,6 +14,17 @@ for line in f:
 twitter = Twython(keys[0], access_token=keys[2]) 
 f.close()
 
+def remove_garbage(inpt):
+  r = re.findall("@[\w]*", inpt)
+  for i in r:
+    inpt = re.sub(i, '', inpt)
+  #remove links
+  inpt = re.sub(r'http\S+', '', inpt)
+  #remove special characters
+  inpt = re.sub(r'[^a-zA-Z ]+', '', inpt)
+  #remove words with 1-2 letters
+  inpt = re.sub(r'\b\w{1,2}\b', '', inpt)
+  return inpt
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -19,9 +32,13 @@ def index():
     req = request.form
     url = req.get('tweet-url').split('/')
     status_id = url[len(url)-1]
-    data =  twitter.lookup_status(id=status_id)
+    data =  twitter.lookup_status(id=status_id, tweet_mode='extended')
     for tweet in data:
-        unfiltered = tweet["text"]
+        unfiltered = tweet["full_text"]
+    unfiltered = unfiltered.replace('\n', ' ')
+    filtered = remove_garbage(unfiltered)
+    # TODO: Run run tweet against model
+    print(filtered)
     return redirect(request.url)
   return render_template('index.html')
 
